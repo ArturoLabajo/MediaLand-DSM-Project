f7fc6dcfdda2776f1fe18e36cb70635cca32fbb0
ruubntz <rbenir02@gmail.com>
2026-03-06 15:28:29 +0100

    Logo en Refresh

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,17 +1,36 @@
-//import { useState } from 'react'
-// import Film/ import FilmDetailView from "./Film/view/FilmDetailView";
-// import ContactView from "./Static/ContactView";
-// import LegalView from "./Static/LegalView";
-// import AuthView from "./Auth/AuthView";
+import { useState, useEffect } from 'react'
+import './App.css';
+//import Film/ import FilmDetailView from "./Film/view/FilmDetailView";
+//import ContactView from "./Static/ContactView";
+//import LegalView from "./Static/LegalView";
+//7import AuthView from "./Auth/AuthView";
+import logo from "./img/logo_001.png";
 
 
 function App() {
 
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 1000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (loading) {
+    return (
+      <div className="splash">
+        <img src={logo} className="logo" alt="logo empresa" />
+      </div>
+    );
+  }
 
   return (
-    <>
-      <h1> Hola </h1>
-    </>
+    
+    <h1> Hola </h1>
+    
   )
 }
 
