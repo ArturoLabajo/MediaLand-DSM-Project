dc94eea4c8ef23b89740c8e0a62f14ab15e5b501
ArturoLabajo <labajo.179482@e.unavarra.es>
2026-03-10 16:49:42 +0100

    Avance arquitectura

--- vite-project/src/Film/infrastructure/FirebaseFilmRepository.tsx
+++ vite-project/src/Film/infrastructure/FirebaseFilmRepository.tsx
@@ -1,12 +1,49 @@
-import type { Film } from "../domain/Film";
+import axios, { type AxiosResponse } from "axios";
 import type { FilmRepository } from "../domain/FilmRepository";
+import type { Film } from "../domain/Film";
 
-export default class FirebaseFilmRepository implements FilmRepository {
-  async getAll(): Promise<Film[]> {
-    throw new Error("Método getAll no implementado todavía");
-  }
+const FirebaseFilmRepository: FilmRepository = {
+    getAll: async () => {
+        const response: AxiosResponse = await axios.get(
+            "https://medialand-ra-default-rtdb.europe-west1.firebasedatabase.app/films.json"
+        );
 
-  async getById(id: string): Promise<Film | null> {
-    throw new Error("Método getById no implementado todavía");
-  }
-}
+        let arrayFilms: Film[] = [];
+
+        for (let key in response.data) {
+            arrayFilms.push({
+                id: key,
+                title: response.data[key].title,
+                releaseDate: response.data[key].releaseDate,
+                director: response.data[key].director,
+                synopsis: response.data[key].synopsis,
+                ratingAverage: response.data[key].ratingAverage,
+                type: response.data[key].type,
+                category: response.data[key].category
+            });
+        }
+
+        return arrayFilms;
+    },
+
+    getById: async (filmId: string) => {
+        const response: AxiosResponse = await axios.get(
+            'https://medialand-ra-default-rtdb.europe-west1.firebasedatabase.app/films.json?orderBy="$key"&equalTo="' + filmId + '"'
+        );
+
+        const film: Film = {
+            id: filmId,
+            title: response.data[filmId].title,
+            releaseDate: response.data[filmId].releaseDate,
+            director: response.data[filmId].director,
+            synopsis: response.data[filmId].synopsis,
+            ratingAverage: response.data[filmId].ratingAverage,
+            type: response.data[filmId].type,
+            category: response.data[filmId].category
+        };
+
+        return film;
+    }
+};
+
+export default FirebaseFilmRepository;
