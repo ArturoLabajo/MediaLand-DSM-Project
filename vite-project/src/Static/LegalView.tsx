d90e2b8bcbff67db29f4735878e6c4a73188b22e
ArturoLabajo <labajo.179482@e.unavarra.es>
2026-03-13 17:10:54 +0100

    Inicio login auth

--- vite-project/src/Static/LegalView.tsx
+++ vite-project/src/Static/LegalView.tsx
@@ -1,6 +1,27 @@
+import { Button, Col, Container, Form, Row } from "react-bootstrap"
+
 function Legal(){
     return(
-        <h1>Informacion legal</h1>
+        <>
+            <Form className="mt-5">
+                <Container>
+                    <Row>
+                        <Col>
+                            <Form.Label> Email: </Form.Label>
+                            <Form.Control type='email' />
+                        </Col>
+
+                        <Col>
+                            <Form.Label> Password: </Form.Label>
+                            <Form.Control type='password' />
+                        </Col>
+                        <Col>
+                            <Button variant='primary'>LOGIN</Button>  
+                        </Col>
+                    </Row>
+                </Container>
+            </Form>
+        </>
     );
 }
 export default Legal;
